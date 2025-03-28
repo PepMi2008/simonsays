abdab751613d59e4b67f233b8d917ebf1a373a0f
pepmiguelburguera <pepmiguelburguera@alumnat.iesdamiahuguet.net>
2025-03-28 13:10:51 +0100

    uep

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-
-const cadacolor =['red','green','blue','purple','orange','yellow','pink','cian'];
+const cadacolor = ['red', 'green', 'blue', 'purple', 'orange', 'yellow', 'pink', 'cian'];
+let posicioAleatoria; //Contindrà un nombre ale
 let colors = [];
+function comensar() {
 
+    document.getElementById('startButton').disabled="true";
+    posicioAleatoria = Math.floor((Math.random() * cadacolor.length));
+    console.log(posicioAleatoria);
+    console.log(cadacolor[posicioAleatoria]);
+    colors.push(cadacolor[posicioAleatoria]);
 
-let posicioAleatoria =  Math.floor((Math.random() * cadacolor.length));
-console.log(posicioAleatoria);
-console.log(cadacolor[posicioAleatoria]);
-colors.push(cadacolor[posicioAleatoria]);
 
 // Mostrar el resultad
-console.log("Color seleccionado:", cadacolor[posicioAleatoria]);
-console.log("Colores seleccionados:", colors);
+    console.log("Color seleccionado:", cadacolor[posicioAleatoria]);
+    console.log("Colores seleccionados:", colors);
+    mostrar_colors(500);
 
-
-
-async function mostrar_colors(temps){
-    for (let i=0;i<colors.length;i++){
-        //TODO instruccions per mostrar el color. Per exemple
-
-        //Crida al mètode esperar
-        await esperar(temps);
-        console.log(colors[i]);
-    }
+//    comensar.disabled = true;
 }
+
+    async function mostrar_colors(temps) {
+        for (let i = 0; i < colors.length; i++) {
+            //TODO instruccions per mostrar el color. Per exemple
+
+            //Crida al mètode esperar
+
+            document.getElementById('red', 'green', 'blue', 'purple', 'orange', 'yellow', 'pink', 'cian').style.backgroundColor='red', 'green', 'blue', 'purple', 'orange', 'yellow', 'pink', 'cian';
+            await esperar(temps);
+            document.getElementById('red', 'green', 'blue', 'purple', 'orange', 'yellow', 'pink', 'cian').style.backgroundColor='red', 'green', 'blue', 'purple', 'orange', 'yellow', 'pink', 'cian';
+
+
+        }
+    }
+
+
 function esperar(milliseconds){
    return new Promise(resolve => {
         setTimeout(resolve, milliseconds);
     });
 }
 
-mostrar_colors(500);
+//mostrar_colors(500);
+
+
+
 
